ddfaeb3b3ce8b44c507794aef2d847ea4be0d3c4
rushdynamic <tameimpalafanclubpresident@gmail.com>
2024-07-10 23:43:02 +0530

    feat: read date arg

--- contrib_flood.sh
+++ contrib_flood.sh
@@ -1,11 +1,13 @@
  #!/bin/bash
 
+formatted_date=$(date -j -f "%d-%m-%Y" "$1" +"%a %b %d 00:00 %Y %z")
+echo ">> Formatted date: $formatted_date"
 cd /Users/gokul/rushd/dummy-flood-repo
 rm README.md
 touch README.md
 git add . && echo ">> Added files"
 git commit -m "test commit" && echo ">> Created commit"
 git push origin main && echo ">> Pushing original commit"
-git commit --amend --date="Fri Feb 16 14:00 2024 +0100" --no-edit && echo ">> Updated author date"
-GIT_COMMITTER_DATE="Fri Feb 16 14:00 2024 +0100" git commit --amend --no-edit && echo ">> Updated committer date"
+git commit --amend --date="$formatted_date" --no-edit && echo ">> Updated author date"
+GIT_COMMITTER_DATE="$formatted_date" git commit --amend --no-edit && echo ">> Updated committer date"
 git push origin main -f && echo ">> Pushed final changes"
