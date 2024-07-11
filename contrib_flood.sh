01fc71fb12b3ce38e2e223fb47a5565ca64b38b9
rushdynamic <tameimpalafanclubpresident@gmail.com>
2024-07-11 17:52:07 +0530

    feat: support date range input

--- contrib_flood.sh
+++ contrib_flood.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-get_dir_and_date() {
+repo_url=$1
+start_date=$2
+end_date=$3
+formatted_dates=()
+
+get_formatted_dates() {
+    if [ -z "$2" ]; then
+        formatted_dates=("$(date -j -f "%d-%m-%Y" "$1" +"%a %b %d 00:00 %Y %z")")
+    else
+        start_sec=$(date -j -f "%d-%m-%Y" "$1" +"%s")
+        end_sec=$(date -j -f "%d-%m-%Y" "$2" +"%s")
+        curr=$start_sec
+        while [ $curr -le $end_sec ]; do
+            formatted_dates+=("$(date -j -f "%s" "$curr" +"%a %b %d %H:%M %Y %z")")
+            curr=$((curr + 86400))
+        done
+    fi
+}
+
+get_git_repo_dir() {
     repo_name=$(echo $1 | awk -F'/' '{ print $NF }' | sed 's/\.git$//')
-    formatted_date=$(date -j -f "%d-%m-%Y" "$2" +"%a %b %d 00:00 %Y %z")
     echo ">> Git repo dir: $repo_name"
-    echo ">> Formatted date: $formatted_date"
 }
 
 clone_repo_if_not_exists() {
@@ -23,19 +40,21 @@ commit_and_push_dummy() {
     touch README.md
     git add . > /dev/null && echo ">> Added files"
     git commit -m "test commit" > /dev/null && echo ">> Created commit"
-    git push origin main > /dev/null && echo ">> Pushing original commit"
 }
 
 update_date() {
-    git commit --amend --date="$formatted_date" --no-edit > /dev/null && echo ">> Updated author date"
-    GIT_COMMITTER_DATE="$formatted_date" git commit --amend --no-edit > /dev/null && echo ">> Updated committer date"
+    git commit --amend --date="$1" --no-edit > /dev/null && echo ">> Updated author date"
+    GIT_COMMITTER_DATE="$1" git commit --amend --no-edit > /dev/null && echo ">> Updated committer date"
     git push origin main -f > /dev/null && echo ">> Pushed final changes"
 }
 
-get_dir_and_date $1 $2
-clone_repo_if_not_exists $1
-commit_and_push_dummy
-update_date
+get_formatted_dates $start_date $end_date
+get_git_repo_dir $repo_url $start_date
+clone_repo_if_not_exists $repo_url
+for date in "${formatted_dates[@]}"; do
+    commit_and_push_dummy
+    update_date "$date"
+done
 
 #TODO:
 # support date range -- if third param is present, perform dummy commits for date range $2 - $3
